c0ce46e4d1502be405e28046c3bba0eb31d4da9e
dbandres <dario26081993@outlook.com.ar>
2024-05-12 22:18:59 -0300

    ejercicios 1 y 2

--- logica/1.js
+++ logica/1.js
@@ -18,5 +18,30 @@
 // {car: 'y', veces: 2}
 
 const contarCar = value => {
-  return
+  let recuento = {};
+
+  for (let i = 0; i < value.length; i++) {
+    let caracter = value[i];
+    recuento[caracter] = (recuento.hasOwnProperty(caracter)) ? recuento[caracter] + 1 : 1;
+  }
+
+  let resultado = [];
+  for (let key in recuento) {
+    resultado.push({ car: key, veces: recuento[key] });
+  }
+
+  let arrayFiltrado = resultado.filter(objeto => {
+    for (let key in objeto) {
+      if (objeto.hasOwnProperty(key) && (objeto[key] === ' ')) {
+        return false;
+      }
+    }
+    return true;
+  });
+  arrayFiltrado.sort((a, b) => a.car.localeCompare(b.car, undefined, { sensitivity: 'base', ignorePunctuation: true }));
+
+  return arrayFiltrado
 }
+let cadena = "Hoy ya es día 10";
+let resultado = contarCar(cadena);
+console.log(resultado);
--- logica/2.js
+++ logica/2.js
@@ -12,6 +12,81 @@
       Salida: MOUREDEV
  */
 
-const t9Keyboard = value => {
-  return
+const teclado = [
+  {
+    '1': ' '
+  },
+  {
+    '2': ['a', 'b', 'c']
+  },
+  {
+    '3': ['d', 'e', 'f']
+  },
+  {
+    '4': ['g', 'h', 'i']
+  },
+  {
+    '5': ['j', 'k', 'l']
+  },
+  {
+    '6': ['m', 'n', 'o']
+  },
+  {
+    '7': ['p', 'q', 'r', 's']
+  },
+  {
+    '8': ['t', 'u', 'v']
+  },
+  {
+    '9': ['w', 'x', 'y', 'z']
+  },
+  {
+    '0': ' '
+  }
+]
+
+function validarBloquesIguales(bloques) {
+  for (let i = 0; i < bloques.length; i++) {
+    let bloque = bloques[i];
+    let primerCaracter = bloque[0];
+    if (!bloque.split("").every(caracter => caracter === primerCaracter)) {
+      return false;
+    }
+  }
+  return true;
 }
+
+function bloquesEnTeclado(bloque) {
+  let caracteres = [];
+  //Debes buscar cuál era su correspondencia original
+  bloque.forEach(bloque => {
+    for (let i = 0; i < teclado.length; i++) {
+      if (teclado[i].hasOwnProperty(bloque[0])) {
+        let index = bloque.length-1
+        caracteres = caracteres.concat(teclado[i][bloque[0]][index])
+      }
+    }
+  });
+  return caracteres
+}
+
+
+
+const t9Keyboard = value => {
+
+  let bloques = value.split('-')
+
+  //- Si un bloque tiene más de un número, debe ser siempre el mismo.
+  if (validarBloquesIguales(bloques)) {
+    const result = bloquesEnTeclado(bloques).join('')
+    return result
+  } else {
+    return "Los bloques no tienen el mismo caracter."
+  }
+
+}
+
+let cadena = "6-666-88-777-33-3-33-888";
+let cadena2 = '44-666-555-2-0-6-88-66-3-666'
+let resultado = t9Keyboard(cadena);
+console.log(resultado);
